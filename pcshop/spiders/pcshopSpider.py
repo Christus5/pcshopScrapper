053e5176635e0ffc102cbf378612ebd13940e843
Christus5 <tavkhelidzeluka@gmail.com>
2021-01-10 21:15:47 +0400

    create notebooks crawler

--- pcshop/spiders/pcshopSpider.py
+++ pcshop/spiders/pcshopSpider.py
@@ -1,5 +1,21 @@
 import scrapy
+from ..items import PcshopItem
+
 
 class PcshopScrapy(scrapy.Spider):
     name = 'pcshop'
-    allowed_domains = ['https://pcshop.ge/']
+    start_urls = [
+        'https://pcshop.ge/product-category/notebooks/'
+    ]
+
+    def parse(self, response, **kwargs):
+        items = PcshopItem()
+        for nb in response.css('main.site-main ul.products li'):
+            items['title'] = nb.css('h2::text').get()
+            items['price'] = nb.css('bdi::text').get()
+
+            yield items
+        
+        next_page = response.css('a.next::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
